f547580b563288124e1477c4740fa5196284cdb5
1sagor34 <1sagor34@gmail.com>
2022-03-28 00:43:53 +0600

    Final term lab

---
+++ Input_y_or_Y.asm
@@ -0,0 +1,29 @@
+.model small
+.stack 100h
+.code
+main proc
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    cmp bl,'y'
+    jz l1
+    
+    cmp bl,'Y'
+    jz l1
+    
+    jnz exit
+    
+    l1:
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+    
+
+
